26daa6c83ed8011f800f27a949477ea786cc0885
osamazhar <osamazhar@yahoo.com>
2016-01-27 11:37:29 +0100

    comments updated

--- Kiel uVision Project Files/Variable Frequency.c
+++ Kiel uVision Project Files/Variable Frequency.c
@@ -9,7 +9,7 @@
 #include <reg51.h>
 
 #define KEYPAD P1												
-#define ldata P0 											//sfr ldata = 0x0B0; for P3
+#define ldata P0 				//sfr ldata = 0x0B0; for P3
 
 sbit rs = P3^5;
 sbit rw = P3^6;
@@ -41,12 +41,12 @@ void main()
 	 unsigned char colloc;
 
 	 Status = 0;
-	 KEYPAD = 0xF0;											// make higher bits of keypad port that is column bits input and low row bits outputs
+	 KEYPAD = 0xF0;			// make higher bits of keypad port that is column bits input and low row bits outputs
 	 Clock = 0;
-	 lcdcmd(0x38);											//2 lines and 5X7 matrix
-	 lcdcmd(0x0C);											//Display ON, Cursor Blinking = 0x0E But this command is Display ON, Cursor OFF
-	 lcdcmd(0x01);											//Clear Display Screen
-	 lcdcmd(0x80);											//Force Cursor to the beginning of the First Line
+	 lcdcmd(0x38);			// 2 lines and 5X7 matrix
+	 lcdcmd(0x0C);			// Display ON, Cursor Blinking = 0x0E But this command is Display ON, Cursor OFF
+	 lcdcmd(0x01);			// Clear Display Screen
+	 lcdcmd(0x80);			// Force Cursor to the beginning of the First Line
 	 lcdcmd(0x06);
 	 
 main:lcdcmd(0x01);
@@ -70,38 +70,38 @@ main:lcdcmd(0x01);
 	 Percent = GetInput();
 	 lcdcmd(0x0C);
 	 TotalDelayMilli = (1 / Frequency) * 1000;
-	 TotalDelayMicro = (TotalDelayMilli * 1000) - 45;		//With correction of 45
+	 TotalDelayMicro = (TotalDelayMilli * 1000) - 45;	//With correction of 45
 	 HighTime = (TotalDelayMicro / 100) * Percent;
 	 LowTime = TotalDelayMicro - HighTime;
 
 	 ValueH = 65536 - HighTime;									 
-	 ConvLH = ValueH & 0x00FF;								 // Zeroing the Higher 8 bits to have Lower 8 bits
-	 LowH = ConvLH;											 // Storing in a 8 bit character
-	 ConvHH = ValueH & 0xFF00;								 // Masking the Lower 8 bits to have Higher 8 bits
-	 ConvHH = ConvHH >> 8;									 // Shifting right 8 bits to hit to the Lower Part to save it to a 8-bit character
-	 HighH = ConvHH;										 // Storing in a 8 bit character
+	 ConvLH = ValueH & 0x00FF;				// Zeroing the Higher 8 bits to have Lower 8 bits
+	 LowH = ConvLH;						// Storing in a 8 bit character
+	 ConvHH = ValueH & 0xFF00;				// Masking the Lower 8 bits to have Higher 8 bits
+	 ConvHH = ConvHH >> 8;					// Shifting right 8 bits to hit to the Lower Part to save it to a 8-bit character
+	 HighH = ConvHH;					// Storing in a 8 bit character
 	 
 	 ValueL = 65536 - LowTime;									
-	 ConvLL = ValueL & 0x00FF;								 // Zeroing the Higher 8 bits to have Lower 8 bits
-	 LowL = ConvLL;											 // Storing in a 8 bit character
-	 ConvHL = ValueL & 0xFF00;								 // Masking the Lower 8 bits to have Higher 8 bits
-	 ConvHL = ConvHL >> 8;									 // Shifting right 8 bits to hit to the Lower Part to save it to a 8-bit character
+	 ConvLL = ValueL & 0x00FF;				// Zeroing the Higher 8 bits to have Lower 8 bits
+	 LowL = ConvLL;						// Storing in a 8 bit character
+	 ConvHL = ValueL & 0xFF00;				// Masking the Lower 8 bits to have Higher 8 bits
+	 ConvHL = ConvHL >> 8;					// Shifting right 8 bits to hit to the Lower Part to save it to a 8-bit character
 	 HighL = ConvHL;									
 	 
 	 Status = 1;
-	 while(1)												//2 MC
+	 while(1)						//2 Machine Cycle
 		{
-		 Clock = 1;											//1 MC
+		 Clock = 1;					//1 MC
 		 TimerH(HighH, LowH);
 		 Clock = 0;
 		 TimerL(HighL, LowL);
-		 colloc = KEYPAD;									//2 MC		 
-	 	 colloc &= 0xF0;									//2 MC		 
-		 if(colloc == 0xE0)									//4 MC
+		 colloc = KEYPAD;				//2 MC		 
+	 	 colloc &= 0xF0;				//2 MC		 
+		 if(colloc == 0xE0)				//4 MC
 		 	{
 	 	 	 MSDelay(5);
-		 	 colloc = KEYPAD;								//read the columns
-		 	 colloc &= 0xF0;								//mask unused bits
+		 	 colloc = KEYPAD;			//read the columns
+		 	 colloc &= 0xF0;			//mask unused bits
 			 if(colloc == 0xE0)
 			 	{
 				 Status = 0;
@@ -109,7 +109,7 @@ main:lcdcmd(0x01);
 				}
 			}												
 		}
-	}														//main braces
+	}							//main braces
 
 unsigned int GetInput()
 	{
@@ -162,7 +162,7 @@ Re:		 Key[cnt] = KeypadRead();
 	}
 
 void MSDelay(unsigned int count) 
-	{// mSec Delay 11.0592 Mhz 
+	{// mSec Delay 12 Mhz 
      unsigned int i;		       		
      while(count)
 		{
@@ -175,12 +175,12 @@ void MSDelay(unsigned int count)
 
 void lcdready()
 	{														
-		busy = 1;	  										//make the busy pin an input
+		busy = 1;	  					//make the busy pin an input
 		rs = 0;
 		rw = 1;
-		while(busy == 1)									//wait here for busy flag
+		while(busy == 1)					//wait here for busy flag
 			{
-				en = 0;										//strobe the enable pin
+				en = 0;					//strobe the enable pin
 				MSDelay(1);
 				en = 1;
 			}
@@ -217,71 +217,71 @@ unsigned char KeypadRead()
 		unsigned char colloc, rowloc; 		
 		do
 		   {												
-		   KEYPAD = 0xF0;									//ground all rows at once
-		   colloc = KEYPAD;									//read the port for columns
-		   colloc &= 0xF0;									//mask row bits
+		   KEYPAD = 0xF0;				// ground all rows at once
+		   colloc = KEYPAD;				// read the port for columns
+		   colloc &= 0xF0;				// mask row bits
 		   }
-		while(colloc != 0xF0);								//check until all keys are released
+		while(colloc != 0xF0);				// check until all keys are released
 		
 		do
 		   {
 		  	 do
 		   		{
-				MSDelay(8);  								//call delay
-				colloc = KEYPAD;							//see if any key is pressed
-				colloc &= 0xF0;								//mask unsused bits
+				MSDelay(8);  			//call delay
+				colloc = KEYPAD;		//see if any key is pressed
+				colloc &= 0xF0;			// mask unsused bits
 				}
-		 	 while(colloc == 0xF0);							//keep checking for keypress
+		 	 while(colloc == 0xF0);			// keep checking for keypress
 
-			MSDelay(8);										//call delay for debounce
-			colloc = KEYPAD;								//read columns
-			colloc &= 0xF0;									//masku unused bits
+			MSDelay(8);				// call delay for debounce
+			colloc = KEYPAD;			// read columns
+			colloc &= 0xF0;				// mask unused bits
 			}
-			while(colloc == 0xF0);							//wait for keypress
+			while(colloc == 0xF0);			// wait for keypress
 
 			while(1)
 				{
-					KEYPAD &= 0xF0;							//masking row bits
-					KEYPAD |= 0x0E;							//now ground row 0 0E = 00001110b ORing won't affect column data
-					colloc = KEYPAD;						//read columns
-					colloc &= 0xF0;							//mask row bits
-					if(colloc != 0xF0)						//column detected
+					KEYPAD &= 0xF0;		// masking row bits
+					KEYPAD |= 0x0E;		// now ground row 0 0E = 00001110b ORing won't affect column data
+					colloc = KEYPAD;	// read columns
+					colloc &= 0xF0;		// mask row bits
+					if(colloc != 0xF0)	// column detected
 						{
-							rowloc = 0;						//save row location
-							break;
+						 rowloc = 0;	// save row location
+						 break;
 						}
 					KEYPAD &= 0xF0;
-					KEYPAD |= 0x0D;							//ground row 1 0D = 00001101b  ORing won't affect column data
-					colloc = KEYPAD;						//read columns
-					colloc &= 0xF0;							//mask row bits
-					if(colloc != 0xF0)						//column detected
+					KEYPAD |= 0x0D;		// ground row 1 0D = 00001101b  ORing won't affect column data
+					colloc = KEYPAD;	// read columns
+					colloc &= 0xF0;		// mask row bits
+					if(colloc != 0xF0)	// column detected
 						{
-							rowloc = 1;						//save row location
-							break;
+						 rowloc = 1;	// save row location
+						 break;
 						}
 					KEYPAD &= 0XF0;
-					KEYPAD |= 0x0B;							//ground row 2 0B = 00001011b
-					colloc = KEYPAD;						//read columns
-					colloc &= 0xF0;							//mask row bits
-					if(colloc != 0xF0)						//column detected
+					KEYPAD |= 0x0B;		// ground row 2 0B = 00001011b
+					colloc = KEYPAD;	// read columns
+					colloc &= 0xF0;		// mask row bits
+					if(colloc != 0xF0)	// column detected
 						{
-							rowloc = 2;						//save row location
-							break;
+						 rowloc = 2;	// save row location
+						 break;
 						}
 					KEYPAD &= 0XF0;
-					KEYPAD |= 0x07;						//ground row 3 07 = 00000111b
-					colloc = KEYPAD;					//read columns
-					colloc &= 0xF0;						//mask row bits
-					if(colloc != 0xF0)					//column detected
-					rowloc = 3;							//save row location
+					KEYPAD |= 0x07;		// ground row 3 07 = 00000111b
+					colloc = KEYPAD;	// read columns
+					colloc &= 0xF0;		// mask row bits
+					if(colloc != 0xF0)	// column detected
+					rowloc = 3;		// save row location
 					break;
 				}
 		//check columns and send result to LCD
-		if(colloc == 0xE0)							//0E = 00001110
+		if(colloc == 0xE0)				// 0E = 00001110
 			return keypad[rowloc][0];
-		else if(colloc == 0xD0)						//0D = 00001101
+		else if(colloc == 0xD0)				// 0D = 00001101
 			return keypad[rowloc][1];
-		else if(colloc == 0xB0)						//0B = 00001011
+		else if(colloc == 0xB0)				// 0B = 00001011
 			return keypad[rowloc][2];
 		else
 			return keypad[rowloc][3];
